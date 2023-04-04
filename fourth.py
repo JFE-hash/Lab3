3ece8e43866f80edec6f169e655d406277898abe
JFE-hash <egorikmenya@gmail.com>
2023-04-04 04:52:01 +0300

    done

---
+++ fourth.py
@@ -0,0 +1,17 @@
+def lucky(num):
+    sum1 = []
+    sum2 = []
+    half = len(num) // 2
+    first_half = num[:half]
+    for i in first_half:
+        sum1.append(int(i))
+    second_half = num[half:]
+    for i in second_half:
+        sum2.append(int(i))
+    if sum(sum1) == sum(sum2):
+        return 'Билет счастливый'
+    else:
+        return 'Билет не счастливый('
+
+
+print(lucky(input()))
